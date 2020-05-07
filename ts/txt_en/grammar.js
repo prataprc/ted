778033e317b8540dc7f2a435677afdb63e1ceee6
prataprc <prataprc@gmail.com>
2020-05-07 11:31:08 +0530

    txt_en: grammar fixes.

--- ts/txt_en/grammar.js
+++ ts/txt_en/grammar.js
@@ -4,9 +4,8 @@ module.exports = grammar({
   rules: {
     source_file: $ => repeat(choice($.word, $.wword, $.dot, $.nl)),
     word: $ => /\w+[\x09\x0b\x0c\x0d\x20]*/,
-    wword: $ => /[^\s\.]+(\.[^\s\.]+)*[\x09\x0b\x0c\x0d\x20]*/,
+    wword: $ => /[^\x09\x0b\x0c\x0d\x20\x0a\.]+(\.[^\x09\x0b\x0c\x0d\x20\x0a\.]+)*[\x09\x0b\x0c\x0d\x20]*/,
     dot: $ => /\.[\x09\x0b\x0c\x0d\x20]*/,
-    nl: $ => /(\x0a\x0d)|(\x0d\x0a)|\x0a/,
-    ws: $ => /[\x09\x0b\x0c\x0d\x20]+/,
+    nl: $ => /(\x0a\x0d)|(\x0d\x0a)|\x0a[\x09\x0b\x0c\x0d\x20]*/,
   }
 });
