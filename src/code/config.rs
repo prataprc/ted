0920e1c8512c9a093336dea5bfc8bc32ce1516d3
prataprc <prataprc@gmail.com>
2020-07-04 12:45:12 +0530

    cleanup.

--- src/code/config.rs
+++ src/code/config.rs
@@ -1,6 +1,6 @@
 use toml;
 
-use std::{convert::TryFrom, convert::TryInto, ffi, fmt, fs, result, str::FromStr};
+use std::{convert::TryFrom, convert::TryInto, ffi, fs, str::FromStr};
 
 use crate::{Error, Result};
 
@@ -14,21 +14,3 @@ config![
     (top_margin_char, char, '-'),
     (color_scheme, String, "default".to_string())
 ];
-
-impl fmt::Display for Config {
-    fn fmt(&self, f: &mut fmt::Formatter) -> result::Result<(), fmt::Error> {
-        write!(
-            f,
-            concat!(
-                "{{ scroll_off = {}, line_number = {}, wrap = {}, ",
-                "left_margin_char = {:?}, top_margin_char = {:?} }}"
-            ),
-            self.scroll_off,
-            self.line_number,
-            self.wrap,
-            self.left_margin_char,
-            self.top_margin_char
-        )?;
-        write!(f, concat!("{{ read_only = {} }}"), self.read_only)
-    }
-}
