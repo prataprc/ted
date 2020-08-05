adbcf658a0fc43cb413058d4a8598c441695a17b
prataprc <prataprc@gmail.com>
2020-08-05 16:08:22 +0530

    main: add a dummy term-loop

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,8 @@ fn main() {
         fs::write("ted-panic.out", strng.as_bytes()).unwrap();
     });
 
+    // println!("{}", term_loop());
+
     match State::new(opts) {
         Ok(state) => match state.event_loop() {
             Ok(outs) => {
@@ -41,4 +43,37 @@ fn main() {
             process::exit(1);
         }
     }
+}
+
+#[allow(unused)]
+fn term_loop() -> String {
+    use crossterm::event::{read, Event, KeyCode, KeyEvent};
+    use crossterm::execute;
+    use crossterm::style::{style, PrintStyledContent};
+    use std::io::{stdout, Write};
+    use ted::term;
+    use ted::util;
+
+    let mut stats = util::Latency::new("EVENT");
+    let mut r_stats = util::Latency::new("READT");
+
+    let tm = term::Terminal::init();
+    loop {
+        let start = std::time::SystemTime::now();
+        let evnt = read().unwrap();
+        r_stats.sample(start.elapsed().unwrap());
+
+        match evnt {
+            Event::Key(KeyEvent { code, modifiers }) => match code {
+                KeyCode::Char('q') => break,
+                KeyCode::Char(ch) => {
+                    //
+                    execute!(stdout(), PrintStyledContent(style(ch.to_string())));
+                }
+                _ => (),
+            },
+            _ => (),
+        }
+    }
+    format!("{}\n", r_stats.pretty_print())
 }
